840673ca23da5b9fbab619ffc65216472fdaa3ea
raidmoooon <yanghc1226@outlook.com>
2019-03-29 02:07:46 +0800

    Add Vector

---
+++ projects/vector_collection/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+    // 访问索引越界 panic
+    // get 越界 None
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let _row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
