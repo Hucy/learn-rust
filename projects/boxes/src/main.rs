3f59b83cabedf11f749a025862fdd238ab438fdc
raidmoooon <yanghc1226@outlook.com>
2019-04-25 20:18:16 +0800

    Update Rc<T>

--- projects/boxes/src/main.rs
+++ projects/boxes/src/main.rs
@@ -3,10 +3,17 @@ enum List {
     Nil,
 }
 
+use std::rc::Rc;
+enum ListRc{
+    Cons(i32, Rc<ListRc>),
+    Nil,
+}
+
+
 use crate::List::{Cons, Nil};
+use crate::ListRc::{Cons as RcCons, Nil as RcNil};
 
 fn main() {
-
     // Box
     let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
@@ -19,22 +26,34 @@ fn main() {
 
     // 函数和方法解引用强制多态
     let name = MyBox::new(String::from("Rust"));
-    hello(&name)
+    hello(&name);
+
+    // 引用计数
+    let a = Rc::new(RcCons(5,Rc::new(RcCons(10,Rc::new(RcNil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = RcCons(3,Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        
+        let _c = RcCons(4,Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 use std::ops::Deref;
 
 struct MyBox<T>(T);
 
-impl <T> MyBox<T> {
-    fn new(x:T) -> MyBox<T> {
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
 impl<T> Deref for MyBox<T> {
     type Target = T;
-    fn deref(&self) -> &T{
+    fn deref(&self) -> &T {
         &self.0
     }
 }
@@ -42,5 +61,3 @@ impl<T> Deref for MyBox<T> {
 fn hello(name: &str) {
     println!("Hello {}", name)
 }
-
-
