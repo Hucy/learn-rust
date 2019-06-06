96ced189a7d8bb51000153e7f537f2d9036a6914
raidmoooon <yanghc1226@outlook.com>
2019-06-06 14:05:37 +0800

    Add Display And Format Example

---
+++ projects/example_collect/display_format/src/main.rs
@@ -0,0 +1,94 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+impl Display for List {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let vec = &self.0;
+        println!("{:#?}", &self);
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            // 对每个元素（第一个元素除外）加上逗号。
+            // 使用 `?` 或 `try!` 来返回错误。
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{}", *color)
+    }
+}
+
+#[derive(Debug)]
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f,"0x{:02X}{:02X}{:02X}",self.red,self.green,self.blue )
+    }
+}
